71d997883a90e0fa27e17d1af5b0751d8909a5b2
khavariali <khavariali@yahoo.com>
2016-12-24 12:03:58 +0800

    Firebase core pod file added

---
+++ Pods/Target Support Files/Pods-Service Provider/Pods-Service Provider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Service_Provider : NSObject
+@end
+@implementation PodsDummy_Pods_Service_Provider
+@end
---
+++ Pods/Target Support Files/Pods-Service Provider/Pods-Service Provider-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Service_ProviderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Service_ProviderVersionString[];
+
